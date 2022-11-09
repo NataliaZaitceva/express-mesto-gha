e8e8c5bf2945c47cf46dc4ddaebf5a9afe466d34
Natalia Zaitceva <sfk89@yandex.ru>
2022-11-09 17:48:07 +0900

    13th project

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,40 +1,36 @@
 const Card = require('../models/card');
 
-const ERROR_CODE = 400;
-const INTERNAL_SERVER_ERROR = 500;
-
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .then((cards) => res.send({ data: cards }))
+    .then((cards) => res.status(200).send({ data: cards }))
     .catch((err) => {
       if (err.name === 'BadRequest') {
-        return res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при создании карточки.' });
+        return res.status(400).send({ message: 'Переданы некорректные данные при создании карточки.' });
       }
-      return res.status(INTERNAL_SERVER_ERROR).send({ message: 'Ошибка работы сервера' });
+      return res.status(500).send({ message: 'Ошибка работы сервера' });
     });
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  console.log(req.user._id);
   Card.create({ name, link })
-    .then((card) => res.send({ data: card }))
+    .then((card) => res.status(200).send({ data: card }, req.user._id))
     .catch((err) => {
       if (err.name === 'BadRequest') {
-        return res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при создании карточки.' });
+        return res.status(400).send({ message: 'Переданы некорректные данные при создании карточки.' });
       }
-      return res.status(INTERNAL_SERVER_ERROR).send({ message: 'Ошибка работы сервера' });
+      return res.status(500).send({ message: 'Ошибка работы сервера' });
     });
 };
 
 module.exports.deleteCardById = (req, res) => {
   Card.findById(req.params.cardId)
-    .then((card) => res.status(201).send({ data: card }))
+    .then((card) => res.status(200).send({ data: card }))
     .catch((err) => {
       if (err.name === 'BadRequest') {
-        return res.status(ERROR_CODE).send({ message: 'Карточка с указанным _id не найдена.' });
+        return res.status(400).send({ message: 'Карточка с указанным _id не найдена.' });
       }
-      return res.status(INTERNAL_SERVER_ERROR).send({ message: 'Ошибка работы сервера' });
+      return res.status(500).send({ message: 'Ошибка работы сервера' });
     });
 };
 
@@ -43,15 +39,15 @@ module.exports.likeCard = (req, res) => Card.findById(
   { $addToSet: { likes: req.user._id } },
   { new: true },
 )
-  .then((card) => res.status(201).send({ data: card }))
+  .then((card) => res.status(200).send({ data: card }))
   .catch((err) => {
     if (err.name === 'BadRequest') {
-      return res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные для постановки лайка' });
+      return res.status(400).send({ message: 'Переданы некорректные данные для постановки лайка' });
     }
     if (!req.user._id) {
-      return res.status(ERROR_CODE).send({ message: 'Передан несуществующий _id карточки.' });
+      return res.status(404).send({ message: 'Передан несуществующий _id карточки.' });
     }
-    return res.status(INTERNAL_SERVER_ERROR).send({ message: 'Ошибка работы сервера' });
+    return res.status(500).send({ message: 'Ошибка работы сервера' });
   });
 
 module.exports.dislikeCard = (req, res) => Card.findById(
@@ -62,10 +58,10 @@ module.exports.dislikeCard = (req, res) => Card.findById(
   .then((card) => res.status(201).send({ data: card }))
   .catch((err) => {
     if (err.name === 'BadRequest') {
-      return res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные для снятия лайка' });
+      return res.status(400).send({ message: 'Переданы некорректные данные для снятия лайка' });
     }
     if (!req.user._id) {
-      return res.status(ERROR_CODE).send({ message: 'Передан несуществующий _id карточки.' });
+      return res.status(404).send({ message: 'Передан несуществующий _id карточки.' });
     }
-    return res.status(INTERNAL_SERVER_ERROR).send({ message: 'Ошибка работы сервера' });
+    return res.status(500).send({ message: 'Ошибка работы сервера' });
   });
--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,8 @@
 const User = require('../models/user');
 
-
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.status(201).send({ data: users }))
+    .then((users) => res.status(200).send({ data: users }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
@@ -16,7 +15,7 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((users) => res.status(201).send({ data: users }))
+    .then((newUser) => res.status(200).send({ data: newUser }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
@@ -27,10 +26,10 @@ module.exports.createUser = (req, res) => {
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.userId)
-    .then((userId) => res.status(201).send({ data: userId }))
+    .then((userId) => res.status(200).send({ data: userId }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return res.status(404).send({ message: 'Запрашиваемый пользователь не найден' });
+        return res.status(400).send({ message: 'Запрашиваемый пользователь не найден' });
       }
       return res.status(500).send({ message: 'Ошибка работы сервера' });
     });
@@ -39,8 +38,8 @@ module.exports.getUserById = (req, res) => {
 module.exports.updateProfile = (req, res) => {
   const { name, about } = req.body;
 
-  User.findByIdAndUpdate({ name, about })
-    .then((updateUser) => res.status(201).send({ data: updateUser }))
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+    .then((updateUser) => res.status(200).send({ data: updateUser }))
     .catch((err) => {
       if (err.name === 'BadRequest') {
         return res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
@@ -56,7 +55,7 @@ module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
 
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
-    .then((updateUser) => res.status(201).send({ data: updateUser }))
+    .then((updateUser) => res.status(200).send({ data: updateUser }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         return res.status(400).send({ message: 'Переданы некорректные данные при обновлении аватара.' });
