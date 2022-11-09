db4bccda06be5b4af9e1cbafd22d56823a567a40
Natalia Zaitceva <sfk89@yandex.ru>
2022-11-09 17:32:27 +0900

    13th project

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,14 @@
 const User = require('../models/user');
 
-const ERROR_CODE = 400;
-const INTERNAL_SERVER_ERROR = 500;
-const NOT_FOUND = 404;
+
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(201).send({ data: users }))
     .catch((err) => {
-      if (err.name === 'BadRequest') {
-        return res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при создании пользователя' });
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
       }
-      return res.status(INTERNAL_SERVER_ERROR).send({ message: 'Ошибка работы сервера' });
+      res.status(500).send({ message: 'Ошибка работы сервера' });
     });
 };
 
@@ -20,21 +18,21 @@ module.exports.createUser = (req, res) => {
   User.create({ name, about, avatar })
     .then((users) => res.status(201).send({ data: users }))
     .catch((err) => {
-      if (err.name === 'BadRequest') {
-        return res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при создании пользователя' });
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
       }
-      return res.status(INTERNAL_SERVER_ERROR).send({ message: 'Ошибка работы сервера' });
+      return res.status(500).send({ message: 'Ошибка работы сервера' });
     });
 };
 
 module.exports.getUserById = (req, res) => {
-  User.findById(req.params.id)
+  User.findById(req.params.userId)
     .then((userId) => res.status(201).send({ data: userId }))
     .catch((err) => {
-      if (err.name === 'BadRequest') {
-        return res.status(NOT_FOUND).send({ message: 'Запрашиваемый пользователь не найден' });
+      if (err.name === 'ValidationError') {
+        return res.status(404).send({ message: 'Запрашиваемый пользователь не найден' });
       }
-      return res.status(INTERNAL_SERVER_ERROR).send({ message: 'Ошибка работы сервера' });
+      return res.status(500).send({ message: 'Ошибка работы сервера' });
     });
 };
 
@@ -45,12 +43,12 @@ module.exports.updateProfile = (req, res) => {
     .then((updateUser) => res.status(201).send({ data: updateUser }))
     .catch((err) => {
       if (err.name === 'BadRequest') {
-        return res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
+        return res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
       }
       if (!req.user._id) {
-        return res.status(ERROR_CODE).send({ message: 'Пользователь с указанным _id не найден.' });
+        return res.status(404).send({ message: 'Пользователь с указанным _id не найден.' });
       }
-      return res.status(INTERNAL_SERVER_ERROR).send({ message: 'Ошибка работы сервера' });
+      return res.status(500).send({ message: 'Ошибка работы сервера' });
     });
 };
 
@@ -60,12 +58,12 @@ module.exports.updateAvatar = (req, res) => {
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
     .then((updateUser) => res.status(201).send({ data: updateUser }))
     .catch((err) => {
-      if (err.name === 'BadRequest') {
-        return res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при обновлении аватара.' });
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные при обновлении аватара.' });
       }
       if (!req.user._id) {
-        return res.status(ERROR_CODE).send({ message: 'Пользователь с указанным _id не найден.' });
+        return res.status(404).send({ message: 'Пользователь с указанным _id не найден.' });
       }
-      return res.status(INTERNAL_SERVER_ERROR).send({ message: 'Ошибка работы сервера' });
+      return res.status(500).send({ message: 'Ошибка работы сервера' });
     });
 };
