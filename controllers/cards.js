15ac5c0cee707b75395fae4cc8e2b67ce3c8cbfe
Natalia Zaitceva <sfk89@yandex.ru>
2022-11-11 14:57:54 +0900

    13th 1

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,11 +2,9 @@ const Card = require('../models/card');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .then((cards) => res.status(200).send({ data: cards }, req.user._id))
+    .then((cards) => res.status(200).send({ data: cards }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(400).send({ message: `${Object.values(err.errors).map((error) => error.message).join(', ')}` });
-      } else {
         res.status(500).send({ message: 'Произошла ошибка' });
       }
     });
@@ -14,8 +12,9 @@ module.exports.getCards = (req, res) => {
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  Card.create({ name, link })
-    .then((card) => res.status(200).send({ data: card }))
+  const owner = req.user._id;
+  Card.create({ name, link, owner })
+    .then((card) => res.status(200).send({ card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: `${Object.values(err.errors).map((error) => error.message).join(', ')}` });
@@ -26,33 +25,55 @@ module.exports.createCard = (req, res) => {
 };
 
 module.exports.deleteCardById = (req, res) => Card.findByIdAndRemove(req.params.cardId)
-  .then((cards) => res.status(200).send(req.params.cardId, { data: cards }))
+  .then((cards) => {
+    if (!cards) {
+      res.status(404).send({ message: 'Карточка с указанным _id не найдена.' });
+    } else {
+      res.status(200).send({ data: cards });
+    }
+  })
   .catch((err) => {
     if (err.name === 'ValidationError') {
       return res.status(400).send({ message: 'Ошибка обработки данных' });
     } return res.status(500).send({ message: 'Ошибка работы сервера' });
   });
 
-module.exports.likeCard = (res, req) => Card.findById(
+module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $addToSet: { likes: req.user._id } },
   { new: true },
 )
-  .then((user) => res.status(200).send(req.user._id, { data: user }))
-  .catch((err) => {
-    if (err.name === 'ValidationError') {
-      res.status(500).send({ message: `${Object.values(err.errors).map((error) => error.message).join(', ')}` });
+  .then((cards) => {
+    if (!cards) {
+      res.status(404).send({ message: 'Передан несуществующий _id карточки.' });
     }
-  });
-
-module.exports.dislikeCard = (res, req) => Card.findById(
-  req.params.cardId,
-  { $pull: { likes: req.user._id } },
-  { new: true },
-)
-  .then((cards) => res.status(200).send(req.params.cardId, { data: cards }))
+    res.status(200).send({ data: cards });
+  })
   .catch((err) => {
     if (err.name === 'ValidationError') {
       res.status(400).send({ message: `${Object.values(err.errors).map((error) => error.message).join(', ')}` });
+    } else {
+      res.status(500).send({ message: 'На сервере произошла ошибка' });
     }
   });
+
+module.exports.dislikeCard = (res, req) => {
+  Card.findById(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((cards) => {
+      if (!cards) {
+        res.status(404).send({ message: 'Передан несуществующий _id карточки.' });
+      }
+      res.status(200).send({ data: cards });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: `${Object.values(err.errors).map((error) => error.message).join(', ')}` });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
+};
--- controllers/users.js
+++ controllers/users.js
@@ -64,7 +64,7 @@ module.exports.updateProfile = (req, res) => {
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
 
-  User.findByIdAndUpdate(req.params.id, { avatar }, { new: true })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
         res.status(404).send({ message: 'Пользователь по указанному id не найден' });
