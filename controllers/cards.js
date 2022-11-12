e2583352e05199bca4af1ba0bd40c711140d5421
Natalia Zaitceva <sfk89@yandex.ru>
2022-11-12 14:15:37 +0900

    13th project

--- controllers/cards.js
+++ controllers/cards.js
@@ -64,3 +64,28 @@ module.exports.likeCard = (req, res) => {
       return res.status(500).send({ message: 'Произошла ошибка сервера' });
     });
 };
+
+module.exports.dislikeCard = (res, req) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .orFail(() => {
+      throw new Error('NotFound');
+    })
+    .then((card) => res.send({ data: card }))
+
+    .catch((err) => {
+      if (err.message === 'NotFound') {
+        return res.status(404).send({ message: 'Передан несуществующий _id карточки.' });
+      }
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Произошла ошибка' });
+      }
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Произошла ошибка' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка сервера' });
+    });
+};
