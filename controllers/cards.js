a75ce65edf9cb8a10076889dc6e2f1416387fb2d
Natalia Zaitceva <sfk89@yandex.ru>
2022-11-14 18:50:31 +0900

    13th project review

--- controllers/cards.js
+++ controllers/cards.js
@@ -19,8 +19,7 @@ module.exports.getCards = (req, res) => {
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  const owner = req.user._id;
-  Card.create({ name, link, owner })
+  Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
