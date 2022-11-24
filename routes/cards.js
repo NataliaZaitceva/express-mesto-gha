522c232636ee70302b99feae28076fad734619eb
Natalia Zaitceva <sfk89@yandex.ru>
2022-11-24 14:33:00 +0900

    14th

--- routes/cards.js
+++ routes/cards.js
@@ -5,12 +5,7 @@ const {
 } = require('../controllers/cards');
 
 router.get('/', getCards);
-router.post('/', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required(),
-  }),
-}), createCard);
+router.post('/', createCard);
 router.delete('/:cardId', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).hex(),
