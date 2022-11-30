c551126d67cb3d266163a1a8053b3ce431c5974f
Natalia Zaitceva <sfk89@yandex.ru>
2022-11-30 11:07:34 +0900

    14th

--- app.js
+++ app.js
@@ -1,46 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const { celebrate, Joi } = require('celebrate');
 const { errors } = require('celebrate');
-const { login, createUser } = require('./controllers/users');
-
 // Слушаем 3000 порт
 const PORT = 3000;
 const app = express();
-const auth = require('./middlewares/auth');
-const NotFoundError = require('./Errors/NotFoundError');
 
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 mongoose.connect('mongodb://localhost:27017/mestodb');
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
-
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), createUser);
-
-// авторизация
-app.use(auth);
-
-// роуты, которым авторизация нужна
-app.use('/users', require('./routes/users'));
-app.use('/cards', require('./routes/cards'));
-
-app.use((req, res, next) => {
-  next(new NotFoundError('Страница не найдена'));
-});
+app.use('/', require('./routes/index'));
 
 app.use(errors());
 
---
+++ routes/index.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const { login, createUser } = require('../controllers/users');
+const auth = require('../middlewares/auth');
+const NotFoundError = require('../Errors/NotFoundError');
+
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+
+router.use(auth);
+
+// роуты, которым авторизация нужна
+router.use('/users', require('./users'));
+router.use('/cards', require('./cards'));
+
+router.use((req, res, next) => {
+  next(new NotFoundError('Страница не найдена'));
+});
+
+module.exports = router;
